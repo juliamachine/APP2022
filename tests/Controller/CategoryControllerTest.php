5419cf6988d7077a880b7bbc58e6e91bf4b4d84c
Julia Misala <maytheswegbeeverinyourfavor@gmail.com>
2022-09-04 19:24:15 +0200

    Testy dla CRUD kategorii

--- tests/Controller/CategoryControllerTest.php
+++ tests/Controller/CategoryControllerTest.php
@@ -25,27 +25,30 @@ class CategoryControllerTest extends WebTestCase
 
     public function testIndex(): void
     {
-        $crawler = $this->client->request('GET', $this->path);
+        $this->client->request('GET', $this->path);
+
+        $fixture = new Category();
+        $fixture->setCreatedAt(new \DateTimeImmutable());
+        $fixture->setUpdatedAt(new \DateTimeImmutable());
+        $fixture->setTitle('My Category');
+
+        $this->repository->add($fixture, true);
+
+        $this->client->request('GET', $this->path);
 
         self::assertResponseStatusCodeSame(200);
-        self::assertPageTitleContains('Category index');
-
-        // Use the $crawler to perform additional assertions e.g.
-        // self::assertSame('Some text on the page', $crawler->filter('.p')->first());
+        self::assertSelectorTextContains("table", "My Category");
     }
 
     public function testNew(): void
     {
         $originalNumObjectsInRepository = count($this->repository->findAll());
 
-        $this->markTestIncomplete();
         $this->client->request('GET', sprintf('%snew', $this->path));
 
         self::assertResponseStatusCodeSame(200);
 
         $this->client->submitForm('Save', [
-            'category[createdAt]' => 'Testing',
-            'category[updatedAt]' => 'Testing',
             'category[title]' => 'Testing',
         ]);
 
@@ -56,59 +59,49 @@ class CategoryControllerTest extends WebTestCase
 
     public function testShow(): void
     {
-        $this->markTestIncomplete();
         $fixture = new Category();
-        $fixture->setCreatedAt('My Title');
-        $fixture->setUpdatedAt('My Title');
-        $fixture->setTitle('My Title');
+        $fixture->setCreatedAt(new \DateTimeImmutable());
+        $fixture->setUpdatedAt(new \DateTimeImmutable());
+        $fixture->setTitle('My Category');
 
         $this->repository->add($fixture, true);
 
         $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
 
         self::assertResponseStatusCodeSame(200);
-        self::assertPageTitleContains('Category');
-
-        // Use assertions to check that the properties are properly displayed.
+        self::assertSelectorTextContains("table", "My Category");
     }
 
     public function testEdit(): void
     {
-        $this->markTestIncomplete();
         $fixture = new Category();
-        $fixture->setCreatedAt('My Title');
-        $fixture->setUpdatedAt('My Title');
-        $fixture->setTitle('My Title');
+        $fixture->setCreatedAt(new \DateTimeImmutable());
+        $fixture->setUpdatedAt(new \DateTimeImmutable());
+        $fixture->setTitle('My Category');
 
         $this->repository->add($fixture, true);
 
         $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
 
         $this->client->submitForm('Update', [
-            'category[createdAt]' => 'Something New',
-            'category[updatedAt]' => 'Something New',
-            'category[title]' => 'Something New',
+            'category[title]' => 'My New Category',
         ]);
 
         self::assertResponseRedirects('/category/');
 
         $fixture = $this->repository->findAll();
 
-        self::assertSame('Something New', $fixture[0]->getCreatedAt());
-        self::assertSame('Something New', $fixture[0]->getUpdatedAt());
-        self::assertSame('Something New', $fixture[0]->getTitle());
+        self::assertSame('My New Category', $fixture[0]->getTitle());
     }
 
     public function testRemove(): void
     {
-        $this->markTestIncomplete();
-
         $originalNumObjectsInRepository = count($this->repository->findAll());
 
         $fixture = new Category();
-        $fixture->setCreatedAt('My Title');
-        $fixture->setUpdatedAt('My Title');
-        $fixture->setTitle('My Title');
+        $fixture->setCreatedAt(new \DateTimeImmutable());
+        $fixture->setUpdatedAt(new \DateTimeImmutable());
+        $fixture->setTitle('My Category');
 
         $this->repository->add($fixture, true);
 
